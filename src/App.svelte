e8cd1421eb41b500ec95d30ad7ced2da0f0e281f
Kidus <kidusm7110@gmail.com>
2025-09-20 12:43:33 +0300

    Inital Commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import '$root/components/Todos.svelte';
+    import '$root/styles/global.css';
+    import Todos from './components/Todos.svelte';
+</script>
+
+<h1>Hello World</h1>
+
+<Todos></Todos>
